e79954d18c24dd785c0bbb8ff6ac0bd96fa5ea2b
Jurij15 <jurij.grom1@gmail.com>
2023-02-02 20:25:38 +0100

    add pyramid

---
+++ Python/Pyramid.py
@@ -0,0 +1,22 @@
+inp = int(input("Enter a number..."))
+
+
+# These helped a bit
+# https://www.knowprogram.com/python/star-pattern-python-while-loop/,(pattern 5)
+# https://www.youtube.com/watch?v=P-mCpH5f3js
+# but i did not just copy/paste it
+
+rows = 0
+while(rows < inp):
+    space = inp-rows-1
+    while space > 0:
+        print(end=" ") #after a long time and many tutorials later,
+        # this end=() function made it work somehow (i am not sure why)
+        # (https://www.toppr.com/guides/python-guide/questions/what-does-end-do-in-python/)
+        space = space-1
+    star = rows+1
+    while(star > 0):
+        print("*", end=" ")
+        star = star-1
+    rows = rows+1
+    print()

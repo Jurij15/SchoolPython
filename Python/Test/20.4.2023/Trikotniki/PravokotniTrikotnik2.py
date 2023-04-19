c78afe0a6e8671806f282c86b7aadead6e013d21
Jurij15 <jurij.grom1@gmail.com>
2023-04-19 18:28:19 +0200

    add even more

---
+++ Python/Test/20.4.2023/Trikotniki/PravokotniTrikotnik2.py
@@ -0,0 +1,16 @@
+visina = int(input("Vnesi visino: "))
+
+RowCounter = 0
+
+while RowCounter < visina:
+    ColumnCounter = 0
+
+    while ColumnCounter < visina:
+        if ColumnCounter < visina - RowCounter - 1:
+            print(" ", end="")
+        else:
+            print("* ", end="")
+        ColumnCounter += 1
+
+    print()
+    RowCounter += 1

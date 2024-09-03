989cace6d935c6d5432d55a3593c05c5e87b95e5
Jure Jurij Grom <79604050+Jurij15@users.noreply.github.com>
2024-09-03 10:52:11 +0200

    add files from 3.9.2024

---
+++ 3L/3.9.2024/1.py
@@ -0,0 +1,24 @@
+﻿# napisi podprogram lihaSt, ki bo vstavil 10 celih števil v polje in ga napolnil zgolj z lihimi števili od 1 do 100
+# program naj nastalo polje izpiše
+
+import random
+
+
+def randomLihoStevilo():
+    x = random.randint(1, 100)
+    while x % 2 == 0:
+        x = random.randint(1, 100)
+
+    return x
+
+
+def lihaSt(y = []):
+    for i in range(0, 10):
+        y.append(randomLihoStevilo())
+
+    return y;
+
+
+x = lihaSt()
+
+print(x)

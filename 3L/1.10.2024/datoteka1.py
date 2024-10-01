a6d7a7ec988140bdeb08e7b868e37040e91cb3bc
Jure Jurij Grom <79604050+Jurij15@users.noreply.github.com>
2024-10-01 11:40:02 +0200

    add files from 1.10.2024

---
+++ 3L/1.10.2024/datoteka1.py
@@ -0,0 +1,20 @@
+﻿file = open("primer.txt", "w")
+file.write("To je nasa prva datoteka. Danes je torek. \n")
+
+file = open("primer.txt", "a")
+file.write("Nase drugo besedilo.")
+
+
+
+file = open("primer.txt", "r")
+print(file.read())
+#file.seek(0)
+
+file = open("primer.txt", "r")
+print(file.readline())
+#file.seek(0)
+
+file = open("primer.txt", "r")
+print(file.readlines())
+
+file.close()

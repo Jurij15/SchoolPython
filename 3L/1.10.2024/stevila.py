9300452086551d63cae57841da3ee7dedb3cdd7f
Jure Jurij Grom <79604050+Jurij15@users.noreply.github.com>
2024-11-05 10:50:09 +0100

    add_imena

---
+++ 3L/1.10.2024/stevila.py
@@ -0,0 +1,22 @@
+﻿with open('stevila.txt', 'r') as file:  # odpiranje datoteke za branje
+    stevila = file.readlines()  # preberemo vsako vrstico in jo shranimo v tabelo
+
+sodaStevila = []  # inicializacija tabele sodaStevila
+lihaStevila = []  # inicializacija tabele lihaStevila
+
+for i in stevila:  # sprehod cez vsa stevila
+    i = i.strip()  # odstrani presledke in znake za novo vrstico
+
+    if i.isdigit():  # preveri, ali je prebrani znak stevilo
+        if int(i) % 2 == 0:  # preveri, ali je stevilo sodo in ga zapise v dolocen seznam
+            sodaStevila.append(i)
+        else:
+            lihaStevila.append(i)
+
+with open('soda.txt', 'w') as file:  # zapisemo vsa soda stevila v datoteko
+    for i in sodaStevila:
+        file.write(i + "\n")
+
+with open('liha.txt', 'w') as file:  # zapisemo vsa liha stevila v datoteko
+    for i in lihaStevila:
+        file.write(i + "\n")
---
+++ 3L/8.10.2024/datoteka2.py
@@ -0,0 +1,18 @@
+﻿#napisi podprogram, ki ustvari datoteko stavki.txt
+#in omogoča vnos besedila
+#ter izpiše vsebino datoteke
+
+##################################
+#To je prvi stavek. TO je drugi  #
+#stavek. To je tretji stavek. To #
+#je četrti stavek                #
+##################################
+
+ime_dat = "stavki.txt"
+#besedilo = input("vnesi besedilo: ")
+
+with open(ime_dat, "w") as file:
+    file.write("To je prvi stavek. TO je drugi \nstavek. To je tretji stavek. To \nje cetrti stavek")
+
+with open(ime_dat, "r") as file:
+    print(file.read())

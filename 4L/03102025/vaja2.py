9129bdc6e94d73b3ef7093f5c2a2b2c9084f6cff
Jure Jurij Grom <79604050+Jurij15@users.noreply.github.com>
2025-10-03 12:24:06 +0200

    add naloge 4l on 3.10.2025

---
+++ 4L/03102025/vaja2.py
@@ -0,0 +1,9 @@
+﻿#napiši funkcijo sortiraj_kopico, ki vrne seznam, urejen z uporabo kopice
+
+import heapq
+
+def sortiraj_kopico(seznam):
+    heapq.heapify(seznam)
+    return seznam
+
+print(sortiraj_kopico([5,3,8,1]))
---
+++ 4L/03102025/vaja3.py
@@ -0,0 +1,8 @@
+﻿#napiši funkcijo k_najmanjših(seznam, k), ki s pomočjo kopice poišče k najmajnjših števil
+import heapq
+
+
+def k_najmanjsih(seznam, k):
+    return heapq.nsmallest(k, seznam)
+
+print(k_najmanjsih([10,2,5,8,1,9], 3))

a7444c7c8b112e1c5de7c45c144433fd6600fc5a
Jure Jurij Grom <79604050+Jurij15@users.noreply.github.com>
2025-10-07 12:46:49 +0200

    add naloge 4l on 7.10.2025

---
+++ 4L/06102025heapnaloge/naloga5.py
@@ -0,0 +1,13 @@
+﻿import heapq
+
+
+def combine(lists):
+    combined = []
+    heapq.heapify(combined)
+    for array in lists:
+        heapq.heapify(array)
+        combined = heapq.merge(combined, array)
+
+    return list(combined)
+
+print(combine([[1,4,7], [2,5,8], [3,6,9]]))
---
+++ 4L/06102025heapnaloge/naloga6.py
@@ -0,0 +1,8 @@
+﻿import heapq
+
+
+def k_najmanjsih(seznam, k):
+    smallest = heapq.nsmallest(k, seznam)
+    return smallest[k-1]
+
+print(k_najmanjsih([7,10,4,3,20,15], 3))

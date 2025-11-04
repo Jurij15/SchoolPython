9a132307e8b9498c0ef67d80cb183ab5c18c6bdb
Jure Jurij Grom <79604050+Jurij15@users.noreply.github.com>
2025-11-04 13:19:55 +0100

    add naloge on 4.11.2025

---
+++ 4L/4.11.2025/10.py
@@ -0,0 +1,25 @@
+﻿def izpisi_meni():
+    print("1.Dodaj ime v seznam")
+    print("2.Izbriši zadnje ime")
+    print("3.Prikaži trenutni seznam")
+    print("4.Konec")
+
+seznam = []
+
+while True:
+    izpisi_meni()
+    izbor = int(input())
+    if izbor == 1:
+        print("Vnesi ime")
+        ime = input()
+        seznam.append(ime)
+    elif izbor == 2:
+        print("izbrisano zadnje ime")
+        seznam.remove(seznam[len(seznam)-1])
+    elif izbor == 3:
+        print(seznam)
+    elif izbor == 4:
+        print("KONEC")
+        break
+    else:
+        print("NAPAČNI IZBOR")
